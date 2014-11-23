756b0ae1c38f5e5f7debe0297676930552473367
Andrew Miller <mail@asmiller.com>
2014-11-23 13:31:30 -0600

    Add audio session

--- src/ios/MainViewController+CDVRemote.m
+++ src/ios/MainViewController+CDVRemote.m
@@ -1,9 +1,22 @@
 #import "MainViewController+CDVRemote.h"
+#import <AVFoundation/AVFoundation.h>
+#import <AudioToolbox/AudioToolbox.h>
 
 @implementation MainViewController (CDVRemote)
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
+
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+
+    NSError *setCategoryError = nil;
+    BOOL success = [audioSession setCategory:AVAudioSessionCategoryPlayback error:&setCategoryError];
+    if (!success) { /* handle the error condition */ }
+
+    NSError *activationError = nil;
+    success = [audioSession setActive:YES error:&activationError];
+    if (!success) { /* handle the error condition */ }
+
 
     // Turn on remote control event delivery
     [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
